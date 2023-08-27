934be8ea1752240adf046902ef94d8da504b1178
idsantri <idsantri.mail@gmail.com>
2023-08-27 07:45:18 +0700

    deploy

--- src/pages/home/HomeIndex.vue
+++ src/pages/home/HomeIndex.vue
@@ -1,7 +1,6 @@
 <template>
 	<div style="margin-top: 50px">
 		<div class="text-subtitle1 text-center">Selamat Datang</div>
-		<div class="text-subtitle2 text-center">di Toko ID Santri</div>
 	</div>
 	<div class="text-center" style="margin-top: 20px">
 		<q-avatar size="150px" square="">
