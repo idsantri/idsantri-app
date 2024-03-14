4cae6d73de40910925a47748d3444de34850a2f4
idsantri <idsantri.mail@gmail.com>
2024-03-14 15:03:06 +0700

    ganti config

--- src/config/constanta.js
+++ src/config/constanta.js
@@ -1,5 +1,5 @@
 const constanta = {
 	APP_NAME_1: 'ID Santri',
-	APP_NAME_2: 'Sistem Informasi Madrasah dan Pesantren',
+	APP_NAME_2: 'Sistem Informasi Pondok Pesantren Syaichona Moh Cholil',
 };
 export default constanta;
