4eeca265a1e89fd1bd05521261ed5f0f87c2db51
idsantri <idsantri.mail@gmail.com>
2024-03-15 04:35:27 +0700

    tes

--- src/config/index.js
+++ src/config/index.js
@@ -2,5 +2,6 @@ const config = {
 	APP_NAME: 'ID Santri Dev',
 	APP_DESCRIPTION: 'Sistem Informasi Madrasah dan Pesantren',
 	BASE_API: 'https://demo.idsantri.my.id/api',
+	APP_VERSION: '1.1',
 };
 export default config;
