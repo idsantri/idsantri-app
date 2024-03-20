5edb1d254c81482d9c6c3ab6a78468dd75ea0eaf
idsantri <idsantri.mail@gmail.com>
2024-03-21 05:10:12 +0700

    print preview --done

--- src/pages/santri/DropDownPrint.vue
+++ src/pages/santri/DropDownPrint.vue
@@ -8,71 +8,127 @@
 		size="sm"
 	>
 		<q-list>
-			<q-item
-				clickable
-				v-close-popup
-				@click="
-					apiDownload({
-						message: 'Download data registrasi?',
-						url: `/reports/santri/registrasi/download?id=${route.params.id}`,
-						fileName: 'registrasi-' + route.params.id,
-					})
-				"
-			>
+			<q-item v-close-popup>
 				<q-item-section>
 					<q-item-label>Registrasi</q-item-label>
 				</q-item-section>
+				<q-item-section avatar>
+					<q-btn icon="download" flat @click="downloadRegistrasi" />
+				</q-item-section>
+				<q-item-section avatar>
+					<q-btn icon="print" flat @click="printRegistrasi" />
+				</q-item-section>
 			</q-item>
-			<q-item
-				clickable
-				v-close-popup
-				@click="
-					apiDownload({
-						message: 'Download standbook?',
-						url: `/reports/santri/standbook/download?id=${route.params.id}`,
-						fileName: 'standbook-' + route.params.id,
-					})
-				"
-			>
+			<q-item v-close-popup>
 				<q-item-section>
 					<q-item-label>Standbook</q-item-label>
 				</q-item-section>
-			</q-item>
-
-			<q-item clickable v-close-popup @click="showDialog = true">
-				<q-item-section>
-					<q-item-label>Permohonan Berhenti</q-item-label>
+				<q-item-section avatar>
+					<q-btn icon="download" flat @click="downloadStandbook" />
+				</q-item-section>
+				<q-item-section avatar>
+					<q-btn icon="print" flat @click="printStandbook" />
 				</q-item-section>
 			</q-item>
 
-			<q-item
-				clickable
-				v-close-popup
-				@click="
-					apiDownload({
-						message: 'Download Keterangan Berhenti',
-						url: `/reports/santri/keterangan-berhenti/download?id=${route.params.id}`,
-						fileName: 'keterangan-berhenti-' + route.params.id,
-					})
-				"
-			>
+			<q-item v-close-popup>
+				<q-item-section>
+					<q-item-label>Permohonan Berhenti</q-item-label>
+				</q-item-section>
+				<q-item-section avatar>
+					<q-btn icon="download" flat disable />
+				</q-item-section>
+				<q-item-section avatar>
+					<q-btn icon="print" flat @click="dialogPermohonan = true" />
+				</q-item-section>
+			</q-item>
+
+			<q-item v-close-popup>
 				<q-item-section>
 					<q-item-label>Keterangan Berhenti</q-item-label>
+				</q-item-section>
+				<q-item-section avatar>
+					<q-btn icon="download" flat @click="downloadKeterangan" />
+				</q-item-section>
+				<q-item-section avatar>
+					<q-btn icon="print" flat @click="printKeterangan" />
 				</q-item-section>
 			</q-item>
 		</q-list>
 	</q-btn-dropdown>
 
-	<q-dialog v-model="showDialog">
-		<PermohonanBerhenti />
+	<q-dialog v-model="dialogPermohonan">
+		<PermohonanBerhenti @submitted="printPermohonan" />
+	</q-dialog>
+	<q-dialog v-model="showViewer">
+		<ReportViewer :url="urlReport" />
 	</q-dialog>
 </template>
 <script setup>
 import apiDownload from 'src/api/api-download';
 import PermohonanBerhenti from './PermohonanBerhenti.vue';
-import { ref } from 'vue';
+import { ref, toRefs } from 'vue';
 import { useRoute } from 'vue-router';
+import ReportViewer from 'src/components/ReportViewer.vue';
+import loadingStore from 'src/stores/loading-store';
+const { loadingMain } = toRefs(loadingStore());
+
 const route = useRoute();
-const showDialog = ref(false);
+const dialogPermohonan = ref(false);
+const urlReport = ref('');
+const showViewer = ref(false);
+
+async function downloadRegistrasi() {
+	await apiDownload({
+		endPoint: `reports/santri/registrasi/download?id=${route.params.id}`,
+		loading: loadingMain,
+		fileName: 'registrasi-' + route.params.id,
+		confirm: true,
+		message: 'Download data registrasi?',
+	});
+}
+
+async function downloadStandbook() {
+	await apiDownload({
+		endPoint: `reports/santri/standbook/download?id=${route.params.id}`,
+		loading: loadingMain,
+		fileName: 'standbook-' + route.params.id,
+		confirm: true,
+		message: 'Download standbook?',
+	});
+}
+
+async function downloadKeterangan() {
+	await apiDownload({
+		endPoint: `reports/santri/keterangan-berhenti/download?id=${route.params.id}`,
+		loading: loadingMain,
+		fileName: 'keterangan-berhenti-' + route.params.id,
+		confirm: true,
+		message: 'Download Keterangan Berhenti',
+	});
+}
+
+function printRegistrasi() {
+	urlReport.value = `reports/santri/registrasi/view?id=${route.params.id}`;
+	showViewer.value = true;
+}
+
+function printStandbook() {
+	urlReport.value = `reports/santri/standbook/view?id=${route.params.id}`;
+	showViewer.value = true;
+}
+
+function printPermohonan(val) {
+	dialogPermohonan.value = false;
+	const queryString = new URLSearchParams(val).toString();
+
+	urlReport.value = `reports/santri/permohonan-berhenti/view?${queryString}`;
+	showViewer.value = true;
+}
+
+function printKeterangan() {
+	urlReport.value = `reports/santri/keterangan-berhenti/view?id=${route.params.id}`;
+	showViewer.value = true;
+}
 </script>
 <style lang=""></style>
