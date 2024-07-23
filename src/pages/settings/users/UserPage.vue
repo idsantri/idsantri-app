51bac2bd7c0b327d32a9873d51528ef93cfd0b6a
idsantri <idsantri.mail@gmail.com>
2024-07-23 09:40:35 +0700

    settings/users

--- src/pages/settings/users/UserPage.vue
+++ src/pages/settings/users/UserPage.vue
@@ -6,7 +6,10 @@
 			row-key="name"
 			:loading="loading"
 			:filter="filter"
-			@row-click="(evt, row, index) => rowClick(evt, row, index)"
+			@row-click="
+				(evt, row, index) => $router.push(`/settings/users/${row.id}`)
+			"
+			:rows-per-page-options="[10, 25, 50, 75, 100, 0]"
 		>
 			<template v-slot:top-left>
 				<div class="text-h6 text-green-10">Data Pengguna</div>
@@ -25,116 +28,16 @@
 				</q-input>
 			</template>
 		</q-table>
-
-		<q-dialog v-model="showUserModal">
-			<q-card style="width: 700px; max-width: 90vw">
-				<q-card-section
-					class="bg-green-8 text-green-11 q-pa-sm row items-center"
-				>
-					<div class="text-subtitle1">Data User</div>
-					<q-space />
-					<q-btn icon="close" flat round dense v-close-popup />
-				</q-card-section>
-
-				<q-card-section class="q-gutter-sm">
-					<q-input
-						standout
-						v-model="user.name"
-						label="Nama"
-						readonly=""
-					/>
-					<q-input
-						standout
-						v-model="user.username"
-						label="Username"
-						readonly=""
-					/>
-					<q-input
-						standout
-						v-model="user.email"
-						label="Email"
-						readonly=""
-					/>
-					<div class="row">
-						<div class="col-md-6 col-sm-12">
-							<q-toggle
-								:model-value="
-									user.email_verified_at ? true : false
-								"
-								label="Verifikasi"
-								disable=""
-								color="green"
-							/>
-							<div class="q-pl-md text-caption">
-								Verifikasi akun hanya bisa dilakukan oleh user
-								yang bersangkutan.
-							</div>
-						</div>
-						<div class="col-md-6 col-sm-12">
-							<q-toggle
-								v-model="user.isConfirmed"
-								label="Konfirmasi"
-								@click="confirmUser(user.isConfirmed)"
-								color="green"
-							/>
-							<div class="q-pl-md text-caption">
-								Konfimasi bahwa Anda mengenal user ini.
-							</div>
-						</div>
-					</div>
-				</q-card-section>
-
-				<!-- roles -->
-				<q-card-section>
-					<div class="row">
-						<div
-							class="col-md-6 col-sm-12"
-							v-for="(value, role) in user.roles"
-							:key="role"
-						>
-							<q-toggle
-								style="min-width: 150px"
-								:label="kebabToTitleCase(role)"
-								:model-value="value"
-								@click="setRole(user.id, role, !value)"
-								color="green"
-							/>
-						</div>
-					</div>
-				</q-card-section>
-
-				<q-card-actions class="bg-green-7 q-pa-sm">
-					<q-btn
-						label="Hapus"
-						color="negative"
-						no-caps=""
-						@click="deleteUser"
-					/>
-					<q-space />
-					<q-btn
-						label="Tutup"
-						no-caps=""
-						color="green-10"
-						v-close-popup
-					/>
-				</q-card-actions>
-			</q-card>
-		</q-dialog>
 	</q-page>
 	<!-- <pre>{{ users }}</pre> -->
 </template>
 <script setup>
-import apiDelete from 'src/api/api-delete';
 import apiGet from 'src/api/api-get';
-import apiUpdate from 'src/api/api-update';
-import { kebabToTitleCase } from 'src/utils/format-text';
 import { onMounted, ref } from 'vue';
 
-const showUserModal = ref(false);
 const filter = ref('');
 const loading = ref(false);
 const users = ref([]);
-const user = ref({});
 
 const columns = [
 	{
@@ -162,16 +65,10 @@ const columns = [
 		// sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
 	},
 	{
-		name: 'group',
-		label: 'Group',
+		name: 'roles',
+		label: 'Group (Role)',
 		align: 'left',
-		field: (row) => {
-			const roles = [];
-			for (const [key, value] of Object.entries(row.roles)) {
-				if (value == true) roles.push(key);
-			}
-			return roles.join(', ');
-		},
+		field: 'roles',
 		sortable: true,
 		// sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
 	},
@@ -207,39 +104,5 @@ async function getUsers() {
 onMounted(async () => {
 	await getUsers();
 });
-
-function rowClick(evt, row /*, index*/) {
-	showUserModal.value = true;
-	user.value = row;
-	user.value.isConfirmed = row.confirmed_at ? true : false;
-}
-
-async function confirmUser(val) {
-	const data = { confirm: val };
-	const id = user.value.id;
-
-	const result = await apiUpdate({
-		endPoint: `users/${id}/confirm`,
-		data,
-	});
-
-	if (!result) return (user.value.isConfirmed = !user.value.isConfirmed);
-
-	await getUsers();
-}
-
-async function deleteUser() {
-	const id = user.value.id;
-	await apiDelete({ endPoint: `users/${id}`, rerender: true });
-}
-
-async function setRole(id, role, value) {
-	user.value.roles[role] = value;
-	const data = { role, value };
-	const update = await apiUpdate({ endPoint: `users/${id}/roles`, data });
-	if (!update) return (user.value.roles[role] = !value);
-
-	await getUsers();
-}
 </script>
 <style></style>
