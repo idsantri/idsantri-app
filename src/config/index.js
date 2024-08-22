3892f71df01772f89a2ff6a6c01040415393d016
idsantri <idsantri.mail@gmail.com>
2024-08-22 09:02:41 +0700

    config

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,9 @@
 const config = {
-	INS_DESC: 'Madrasah &',
+	INS_DESC: 'Madrasah dan ',
 	INS_NAME: 'Pondok Pesantren',
 	INS_SHORT: 'Demo Aplikasi',
-	BASE_API: 'https://demo.idsantri.my.id/api',
+	BASE_API: 'https://demo.idsantri.my.id',
+	END_API: '/api',
 	DEV: true,
 };
 export default config;
