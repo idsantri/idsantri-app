c357eee2767957258664189cbb63eaceb7a89fb6
idsantri <idsantri.mail@gmail.com>
2024-08-23 12:08:02 +0700

    add end_api to config

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,8 @@ const config = {
 	INS_DESC: 'Pondok Pesantren',
 	INS_NAME: 'Syaichona Moh Cholil',
 	INS_SHORT: 'Syaichona',
-	BASE_API: 'https://syaichona.idsantri.my.id/api',
+	BASE_API: 'https://syaichona.idsantri.my.id',
+	END_API: '/admin',
 	DEV: false,
 };
 export default config;
